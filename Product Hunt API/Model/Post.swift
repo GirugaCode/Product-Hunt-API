291acb279a0661ce476bd88bbfe7ef37bcc2a47e
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-29 10:58:12 -0800

    [add] Post Model

---
+++ Product Hunt API/Model/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Product Hunt API
+//
+//  Created by Ryan Nguyen on 1/29/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
