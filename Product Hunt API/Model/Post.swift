48b96187e178b27c308f21d5a564e3f4c21e3264
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-29 14:57:34 -0800

    [update] Decodable model

--- Product Hunt API/Model/Post.swift
+++ Product Hunt API/Model/Post.swift
@@ -14,4 +14,35 @@ struct Post {
     let tagline: String
     let votesCount: Int
     let commentsCount: Int
+    let previewImageURL: URL
+}
+
+struct PostList: Decodable {
+    var posts: [Post]
+}
+
+extension Post: Decodable {
+    enum PreviewImageURLKeys: String, CodingKey {
+        case imageURL = "850px"
+    }
+    enum PostKeys: String, CodingKey {
+        case id
+        case name
+        case tagline
+        case votesCount = "votes_count"
+        case commentsCount = "comments_count"
+        case previewImageURL = "screenshot_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let postsContainer = try decoder.container(keyedBy: PostKeys.self)
+        id = try postsContainer.decode(Int.self, forKey: .id)
+        name = try postsContainer.decode(String.self, forKey: .name)
+        tagline = try postsContainer.decode(String.self, forKey: .tagline)
+        votesCount = try postsContainer.decode(Int.self, forKey: .votesCount)
+        commentsCount = try postsContainer.decode(Int.self, forKey: .commentsCount)
+        
+        let screenshotURLContainer = try postsContainer.nestedContainer(keyedBy: PreviewImageURLKeys.self, forKey: .previewImageURL)
+        previewImageURL = try screenshotURLContainer.decode(URL.self, forKey: .imageURL)
+    }
 }
